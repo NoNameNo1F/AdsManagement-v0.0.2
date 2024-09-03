11945165eb430f5ad6db8495e305fe4eb7afe77d
20127670 <nonameno1f@gmail.com>
2024-09-04 00:21:08 +0700

    feat: adding container for right-aside showing AdsPoints

---
+++ src/components/page/adspoint/AdsContainer.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import "./AdsPoint.css";
+import AdsPointContainer from './AdsPointContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../store/redux/store';
+import { handleAdsContainer, } from '../../../store/redux/AdsPoint/actions';
+
+
+const AdsContainer: React.FC = () => {
+    const dispatch = useDispatch();
+    const isVisible: boolean = useSelector((state: RootState) => state.adsPoint.showAds);
+    const title: string = useSelector((state: RootState) => state.adsPoint.adsTitle);
+
+    // const handleShow = () => {
+    //     dispatch(handleAdsContainer());
+    // };
+
+    return (
+        <div className={`ads-container ${isVisible ? 'ads-container--show' : 'ads-container--hide'}`}>
+            <div className="ads-header">
+                <button className={`ads-icon-button${isVisible ? "--expanded" : ""}`} onClick={() => dispatch(handleAdsContainer())}>
+                    <i className={`bi ${isVisible ? 'bi-chevron-right' : 'bi-chevron-left'}`}></i>
+                </button>
+                <span className={`ads-title ${isVisible ? 'ads-title--show' : 'ads-title--hide'}`}>
+                    {title}
+                </span>
+            </div>
+            {isVisible && (
+                <AdsPointContainer />
+            )}
+        </div>
+    );
+};
+
+export default AdsContainer;
