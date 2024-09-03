bcdab82ace428e4e825dbd0305420f48cc3826a4
20127670 <nonameno1f@gmail.com>
2024-09-04 00:13:47 +0700

    feat(UI): adding Footer.

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+  return (
+    <div className="footerr">
+      20127670 - 20127663 self-project. adsmanagement.
+    </div>
+  );
+};
+export default Footer
