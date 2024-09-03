c9aaec898ac64d29866fc1b8df8bd101fb6a9aa5
20127670 <nonameno1f@gmail.com>
2024-09-04 00:22:52 +0700

    feat: adding AdsPointContainer to display actions requirement.

---
+++ src/components/page/adspoint/AdsPointContainer.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import "./AdsPoint.css";
+import AdsPointsList from './AdsPointsList';
+import AdsPointItemDetail from './AdsPointItemDetail';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/redux/store';
+import { IAdsPointItem } from '../../../interfaces';
+
+const AdsPointContainer: React.FC = () => {
+
+    const selectedAdsPointItem: IAdsPointItem = useSelector((state: RootState) => state.adsPoint.selectedAdsPointItem);
+    {/* {
+        showOptions === "newest" ? <AdsPointList option="newest" /> :
+            showOptions === "hot" ? <AdsPointList option="hot" /> :
+                showOptions === "nearby" ? <AdsPointList option="nearby" /> :
+                    <AdsPointList option="newest" />
+    } */}
+
+    return (
+        <div className="ads-point-container">
+            {selectedAdsPointItem ? (<AdsPointItemDetail {...selectedAdsPointItem} />) : (<AdsPointsList />)}
+        </div>
+    );
+};
+
+export default AdsPointContainer;
